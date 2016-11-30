61357cbe8f783c453a83007b1868af3144b5f99f
GabrielSVinha <gabriel.vinha@ccc.ufcg.edu.br>
2016-11-30 12:38:41 -0300

    Finish refactoring components

--- lab02/src/app/app.component.ts
+++ lab02/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { MenuComponent } from './menu/menu.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
